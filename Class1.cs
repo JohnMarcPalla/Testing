dfc2f2a42933fe499a1a52db62676dd011ab2db9
John Marc Palla <johnmarcpalla@gmail.com>
2020-11-02 03:54:26 +0800

    Testing
    Mga kabalbalan ni maki

---
+++ Class1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Testing
+{
+    class Class1
+    {
+        public void simpleFunc(string name)
+        {
+            Console.WriteLine("Hello, " + name); 
+        }
+        public void stackIn(object obj, Stack stack)
+        {
+            stack.Push(obj);
+            Console.WriteLine("Successfully pushed object into stack");
+        }
+        public void stackOut(Stack stack)
+        {
+            object x = stack.Pop();
+            Console.WriteLine("Successfully removed {0} from the stack",x );
+        }
+        public void stackMove(Stack stackIn, Stack stackOut)
+        {
+            stackIn.Push(stackOut.Pop());
+        }
+        public void displayStacks(Stack s1, Stack s2)
+        {
+            int x = 0;
+            Console.WriteLine("Stack 1");
+            foreach (var item in s1)
+            {
+                Console.WriteLine("[{0}] " + item, s1.Count-x);
+                x = x+1;
+            }
+            x = 0;
+            Console.WriteLine("\n\nStack 2");
+            foreach (var items in s2)
+            {
+                Console.WriteLine("[{0}] " + items, s2.Count - x);
+                x = x + 1;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Name: ");
+            //new Class1().simpleFunc(Console.ReadLine());
+            //Console.ReadLine();
+            Stack stack1 = new Stack();
+            Stack stack2 = new Stack();
+            while (true)
+            {
+                Console.Write("[1]Push \n[2]Pop\n[3]Display stacks\nChoice: ");
+                int choice = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (choice == 1)
+                {
+                    
+                    Console.Write("Enter Value to be stored in the Stack: ");
+                    object objects = Console.ReadLine();
+                    Console.Write("Choose which stack to store: ");
+                    int choice1 = int.Parse(Console.ReadLine());
+                    if (choice1 == 1)
+                    {
+                        new Class1().stackIn(objects, stack1);
+                        Console.ReadLine();
+                    }
+                    else if (choice1 == 2)
+                    {
+                        new Class1().stackIn(objects, stack2);
+                        Console.ReadLine();
+                    }
+                    Console.Clear();                  
+                }
+                else if (choice == 2)
+                {
+                    Console.Write("Which stack to be removed?:  ");
+                    int choices = int.Parse(Console.ReadLine());
+                    if (choices == 1)
+                    {
+                        new Class1().stackOut(stack1);
+                    }
+                    else if (choices == 2)
+                    {
+                        new Class1().stackOut(stack2);
+                    }
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (choice == 3)
+                {
+                    new Class1().displayStacks(stack1, stack2);
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+
+                Console.Clear();
+            }
+            
+        }
+    }
+}
